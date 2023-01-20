ea74195b6b1ae1288c4851f0243e231d4a29f3ba
Lucho <bravo.luis.1995@gmail.com>
2023-01-19 20:26:06 -0500

    Por hacer el puntaje de la filosofia del booyah

--- prueba2.py
+++ prueba2.py
@@ -5,6 +5,19 @@ import pandas as pd
 df = pd.read_csv("data_partida.csv", header=0)
 name = list(df["nickname"])
 teams = list(df["team_name"])
+total_kills = list(df["killing_score"])
+assists = list(df["assists"])
+damages = list(df["damage"])
+kills = list(df["kills"])
+med_kit = list(df["medkit_use"])
+team_mates_revived = list(df["revive_teammate_times"])
+knock_down = list(df["knock_down"])
+headshots = list(df["headshots"])
+grenades = list(df["grenade_use"])
+survival_time = list(df["survival_time"])
+booyah = list(df["booyah"])
+score = list(df["ranking_score"])
+revived_times = list(df["revived_times"])
 
 
 def maximum(items, word):
@@ -29,19 +42,19 @@ def maximum(items, word):
 
     max_item = heapq.nlargest(3, zip(items, name))
     names = []
-    kills = []
+    achievements = []
     index = 0
     for item in max_item:
         names.append(item[1])
-        kills.append(item[0])
+        achievements.append(item[0])
         index += 1
 
     print("Los 3 jugadores con mas {} son:\n"
           "{} con {} {}\n"
           "{} con {} {}\n"
-          "{} con {} {}\n".format(object_sing, names[0], kills[0], object_sing,
-                                  names[1], kills[1], object_sing,
-                                  names[2], kills[2], object_sing))
+          "{} con {} {}\n".format(object_sing, names[0], achievements[0], object_sing,
+                                  names[1], achievements[1], object_sing,
+                                  names[2], achievements[2], object_sing))
 
 
 def minimum(items):
@@ -107,39 +120,124 @@ def maximum_for_teams(items, team_name, word):
     max_item = heapq.nlargest(1, zip(items, team_name))
 
     print("{} es el equipo con mas {}, en total {}.".format(max_item[0][1], object_sing, max_item[0][0]))
-    return max_item[0][0]
+    return max_item
 
 
-def max_total_kills(total_kills, damages):
+def get_players_alive(survival_time_n):
+
+    max_survival_time = max(survival_time_n[0])
+
+    items_arr = np.array(survival_time_n[0])
+    a = np.where(items_arr == max_survival_time)
+
+    print("En el equipo de {}".format(survival_time_n[1]))
+
+    if len(a[0]) == 4:
+        print("Todos los jugadores quedaron vivos, el maximo tiempo de supervivencia fue {} segundos".format(max_survival_time))
+        return 4, max_survival_time
+    elif len(a[0]) == 3:
+        print("3 jugadores quedaron vivos, el maximo tiempo de supervivencia fue {} segundos".format(max_survival_time))
+        return 3, max_survival_time
+    elif len(a[0]) == 2:
+        print("2 jugadores quedaron vivos, el maximo tiempo de supervivencia fue {} segundos".format(max_survival_time))
+        return 2, max_survival_time
+    else:
+        print("1 jugador quedo vivo, el maximo tiempo de supervivencia fue {} segundos".format(max_survival_time))
+        return 1, max_survival_time
+
+
+def get_statistics_for_best_teams(item, teams_new, positions, word):
+    object_sing = "content"
+    statistics_teams = item
+    if word == "kills":
+        object_sing = "ELIMINACIONES"
+    elif word == "damage":
+        object_sing = "DAÑOS"
+    elif word == "damage":
+        object_sing = "daño"
+    elif word == "resurrects":
+        object_sing = "RESURRECCIONES"
+        statistics_teams = get_sum_item_for_teams(item)
+
+    statistic_total_list = [[statistics_teams[teams_new.index(positions[0][1])], positions[0][1]],
+                          [statistics_teams[teams_new.index(positions[1][1])], positions[1][1]],
+                          [statistics_teams[teams_new.index(positions[2][1])], positions[2][1]]]
+    print("{} DE LOS EQUIPOS ".format(object_sing), statistic_total_list)
+    return statistic_total_list
+
+
+def get_time_survival_and_survivors(teams_new, positions):
+    e = 0
+    f = 4
+    survival_for_team = []
+    for c in range(12):
+        d = slice(e, f)
+        survival_for_team.append(survival_time[d])
+        e += 4
+        f += 4
+
+    players_alive_list = []
+    max_survival_time_list = []
+
+    for index in range(0, 3):
+        survival_time_new = [survival_for_team[teams_new.index(positions[index][1])], positions[index][1]]
+        players_alive, max_survival_time = get_players_alive(survival_time_new)
+        players_alive_list.append(players_alive)
+        max_survival_time_list.append(max_survival_time)
+
+    print(players_alive_list, max_survival_time_list)
+    return players_alive_list, max_survival_time_list
+
+
+def xb_philosophy(score_list, teams_new, damage_total_list, total_kills_list):
+    positions = heapq.nlargest(3, zip(score_list, teams_new))
+    print("POSICIONES DE LOS EQUIPOS", positions)
+
+    # Obteniendo las veces que usaron la resurreccion
+    revived_total_list = get_statistics_for_best_teams(revived_times, teams_new, positions, "resurrects")
+
+    damage_for_bests_teams = get_statistics_for_best_teams(damage_total_list, teams_new, positions, "damage")
+
+    kills_for_bests_teams = get_statistics_for_best_teams(total_kills_list, teams_new, positions, "kills")
+
+    num_players_alive, max_survival_time = get_time_survival_and_survivors(teams_new, positions)
+
+    # Tiempo de supervivencia por equipo
+
+
+def teams_statistics():
     # Sacando una tabla de los 12 equipos
     teams_new = get_one_item_for_teams(teams)
-    print(teams_new)
 
     # Sacando las kills totales por cada equipo
     total_kills_list = get_one_item_for_teams(total_kills)
-    print(total_kills_list)
 
     # Obteniendo el equipo con más kills
     kills_total = maximum_for_teams(total_kills_list, teams_new, "kills")
 
-    # Sacando el daño total por equipo
+    # Obteniendo el equipo com mas daño
     damage_total_list = get_sum_item_for_teams(damages)
-    print(damage_total_list)
     total_damage = maximum_for_teams(damage_total_list, teams_new, "damage")
+
+    # Sacando el daño total por equipo
+    score_list = get_one_item_for_teams(score)
+    # Daño máximo de partida
+    highest_damages_teams = heapq.nlargest(3, zip(damage_total_list, teams_new))
+    print("DAÑO MÁS ALTOS", highest_damages_teams)
+
+    kills_for_bests_teams = heapq.nlargest(3, zip(total_kills_list, teams_new))
+
+    print("EQUIPOS CON MÁS KILLS", kills_for_bests_teams)
+
+    titulo = "EXPECTATIVE BOOYAH PHILOSOPHY"
+    print("\n" + titulo + "\n" + "-" * len(titulo) + "\n")
+
+    xb_philosophy(score_list, teams_new, damage_total_list, total_kills_list)
 
 
 def main():
-
-    total_kills = list(df["killing_score"])
-    assists = list(df["assists"])
-    damages = list(df["damage"])
-    kills = list(df["kills"])
-    med_kit = list(df["medkit_use"])
-    team_mates_revived = list(df["revive_teammate_times"])
-    knock_down = list(df["knock_down"])
-    headshots = list(df["headshots"])
-    grenades = list(df["grenade_use"])
-    survival_time = list(df["survival_time"])
+    titulo = "DATOS POR JUGADORES, ELIGIENDO LOS MEJORES 3"
+    print("\n" + titulo + "\n" + "-" * len(titulo) + "\n")
 
     # Jugador con menos tiempo de supervivencia
     minimum(survival_time)
@@ -167,8 +265,11 @@ def main():
     # Sacando el jugador con más asistencias
     maximum(assists, "assists")
 
+    titulo = "DATOS POR EQUIPOS"
+    print("\n" + titulo + "\n" + "-" * len(titulo) + "\n")
+
     # Sacando estadísticas de los equipos en conjunto
-    max_total_kills(total_kills, damages)
+    teams_statistics()
 
 
 if __name__ == "__main__":
